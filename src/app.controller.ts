6eb6b50010e8ddeeb6fd0648ab6bd4a068030a34
JanLimpensG <jan_limpens@hotmail.com>
2021-12-05 11:42:27 -0600

    feat: original commit

---
+++ src/app.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return 'Hola Mundo!';
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'Nuevo endpoint xd';
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `Yo soy un filter`;
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:categoryId/products/:productId')
+  getCategory(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ) {
+    return `product ${categoryId} and ${productId}`;
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit}, offset => ${offset}, brand => ${brand}`;
+  }
+}
